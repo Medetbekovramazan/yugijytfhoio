550f6776536d41b1bd1cfd274d458872db24f223
Medetbekovramazan <144230930+Medetbekovramazan@users.noreply.github.com>
2023-10-04 17:57:41 +0300

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,63 @@
+#s = input()
+#print(s[2])
+#print(s[-1])
+#print(s[:5])
+#print()
+#6
+#s = input()
+#k = s.count(' ')
+#print(k+1)
+#7
+#string = input("Введите строку: ")
+#print("Третий символ строки:", string[2])
+#print("Предпоследний символ строки:", string[-2])
+#print("Первые пять символов строки:", string[:5])
+#print("Строка без последних двух символов:", string[:-2])
+#print("Символы с четными индексами:", string[::2])
+#print("Символы с нечетными индексами:", string[1::2])
+#print("Строка в обратном порядке:", string[::-1])
+#print("Символы через один в обратном порядке:", string[::-2])
+#print("Длина строки:", len(string))
+#8
+#string = input("количество слов: ")
+#word_count = string.count(' ') + 1
+#print("Количество слов в строке:", word_count)
+#print()
+
+#string = input("две половинки: ")
+#half_length = len(string) // 2
+#new_string = string[half_length:] + string[:half_length]
+#print("Новая строка:", new_string)
+
+#string = input("Первое и последнее вхождения: ")
+#print('если -1 то это означает что тут нет \'f\'')
+#print(string.find('f'))
+#print(string.rfind('f'))
+#9
+#string = input("Удаление фрагмента: ")
+#main = string.find('h')
+#froom = string.rfind('h')
+#print(string[:main] + string[froom+1:])
+
+#string = input("Введите строку: ")
+#new_string = string.replace('1', 'опе')
+#print("Новая строка:", new_string)
+
+dalete=input('Удаление символа @:   ')
+print(dalete.replace('@', ''))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
